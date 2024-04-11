0deb8f9a6663176a858fe8aa07738718108c8e0f
Laura Harper <lauraharper95@gmail.com>
2024-04-11 13:33:18 +0100

    added constructor and getters and setters to employees class

--- backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/models/Employee.java
+++ backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/models/Employee.java
@@ -1,9 +1,11 @@
 package com.example.HR_System_Backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "employees")
@@ -44,13 +46,147 @@ public class Employee {
     @Column(name = "active")
     private Boolean active;
 
+    @JsonIgnoreProperties({"employee"})
     @OneToMany(mappedBy = "employee")
     private List<Employee> managees;
 
+    @JsonIgnoreProperties({"managees"})
     @ManyToOne
     @JoinColumn(name = "manager_id")
     private Employee manager;
 
+    @JsonIgnoreProperties({"employee"})
     @OneToMany(mappedBy = "employee")
-    private List<RequestedTimeOffs> requestedTimeOffs;
+    private List<RequestedTimeOff> requestedTimeOffs;
+
+    public Employee() {}
+
+    public Employee(String firstName, String lastName, String workEmail, String password, Period totalHoliday,
+                    int sortCode, int accountNumber, int salary, LocalDate employeeStartDate, Employee manager) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.workEmail = workEmail;
+        this.password = password;
+        this.totalHoliday = totalHoliday;
+        this.sortCode = sortCode;
+        this.accountNumber = accountNumber;
+        this.salary = salary;
+        this.employeeStartDate = employeeStartDate;
+        this.active = true;
+        this.managees = new ArrayList<>();
+        this.manager = manager;
+        this.requestedTimeOffs = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getWorkEmail() {
+        return workEmail;
+    }
+
+    public void setWorkEmail(String workEmail) {
+        this.workEmail = workEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Period getTotalHoliday() {
+        return totalHoliday;
+    }
+
+    public void setTotalHoliday(Period totalHoliday) {
+        this.totalHoliday = totalHoliday;
+    }
+
+    public int getSortCode() {
+        return sortCode;
+    }
+
+    public void setSortCode(int sortCode) {
+        this.sortCode = sortCode;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getEmployeeStartDate() {
+        return employeeStartDate;
+    }
+
+    public void setEmployeeStartDate(LocalDate employeeStartDate) {
+        this.employeeStartDate = employeeStartDate;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public List<Employee> getManagees() {
+        return managees;
+    }
+
+    public void setManagees(List<Employee> managees) {
+        this.managees = managees;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public List<RequestedTimeOff> getRequestedTimeOffs() {
+        return requestedTimeOffs;
+    }
+
+    public void setRequestedTimeOffs(List<RequestedTimeOff> requestedTimeOffs) {
+        this.requestedTimeOffs = requestedTimeOffs;
+    }
 }
