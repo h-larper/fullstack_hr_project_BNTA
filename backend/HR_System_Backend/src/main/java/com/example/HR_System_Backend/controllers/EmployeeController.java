e680e97871296cb3fd2575c0b153939c08c75ef7
Karen Yip <karenyip1999@gmail.com>
2024-04-11 15:09:37 +0100

    Created getAllEmployees method

--- backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/controllers/EmployeeController.java
+++ backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/controllers/EmployeeController.java
@@ -1,9 +1,16 @@
 package com.example.HR_System_Backend.controllers;
 
+import com.example.HR_System_Backend.models.Employee;
 import com.example.HR_System_Backend.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/employees")
@@ -11,4 +18,8 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
 }
--- backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/services/EmployeeService.java
+++ backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/services/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.example.HR_System_Backend.services;
 
+import com.example.HR_System_Backend.models.Employee;
 import com.example.HR_System_Backend.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
@@ -10,6 +13,9 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
+    public List<Employee> getAllEmployees () {
+        return employeeRepository.findAll();
+    }
 
 
 }
