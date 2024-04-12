6efd3d5cd04c8ada0c720717ab2574617f0db611
Laura Harper <lauraharper95@gmail.com>
2024-04-12 10:51:48 +0100

    seeded data in dataloader for timeOffs

--- backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/components/DataLoader.java
+++ backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/components/DataLoader.java
@@ -1,11 +1,14 @@
 package com.example.HR_System_Backend.components;
 
 import com.example.HR_System_Backend.models.Employee;
+import com.example.HR_System_Backend.models.RequestedTimeOff;
+import com.example.HR_System_Backend.models.Type;
 import com.example.HR_System_Backend.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestScope;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -22,20 +25,20 @@ public class DataLoader implements ApplicationRunner {
 
         //Managers
         Employee employee1 = new Employee("Lily", "Bobilly", "lily@lily.com",
-                "humanssuck", 365, 000000, 88888888, 20,
+                "humanssuck", 0, 000000, 88888888, 200000000,
                 LocalDate.of(2023,10,6));
         employeeRepository.save(employee1);
 
-        Employee employee2 = new Employee("Laura", "Harper", "laura@laura.com",
-                "cats", 20, 451269, 12345678, 200,
+        Employee employee2 = new Employee("Laura", "Harper", "laura@lily.com",
+                "cats", 20, 451269, 12345678, 2,
                 LocalDate.of(2023,10,6));
         employeeRepository.save(employee2);
         employee2.setManager(employee1);
         employeeRepository.save(employee2);
 
         //Managees
-        Employee employee3 = new Employee("Hannah", "Riley", "hannah@hannah.com",
-                "dogs", 200, 123456, 87654321, 200000,
+        Employee employee3 = new Employee("Hannah", "Riley", "hannah@lily.com",
+                "dogs", 200, 123456, 87654321, 600000,
                 LocalDate.of(2024,4,10));
         //Save employee so it gets an id
         employeeRepository.save(employee3);
@@ -44,19 +47,46 @@ public class DataLoader implements ApplicationRunner {
         //Update the employee
         employeeRepository.save(employee3);
 
-        Employee employee4 = new Employee("Maya", "Tetteh", "maya@maya.com",
+        Employee employee4 = new Employee("Maya", "Tetteh", "maya@lily.com",
                 "orcas", 364, 246810, 12345679, 3000000,
                 LocalDate.of(2001,12,25));
         employeeRepository.save(employee4);
         employee4.setManager(employee2);
         employeeRepository.save(employee4);
 
-        Employee employee5 = new Employee("Karen", "Yip", "karen.karen@karen.com",
+        Employee employee5 = new Employee("Karen", "Yip", "karen.karen@lily.com",
                 "flapjack", 2, 147258, 98745612, 500,
                 LocalDate.of(2021,10,25));
         employeeRepository.save(employee5);
         employee5.setManager(employee2);
         employeeRepository.save(employee5);
 
+        //REQUESTED TIME OFFS
+
+        RequestedTimeOff requestedTimeOff1 = new RequestedTimeOff(LocalDate.of(2024,04,01),
+                LocalDate.of(2024,04, 8), Type.HOLIDAYLEAVE, "Need a break!!!!!",
+                employee5);
+//        requestedTimeOffRepository.save(requestedTimeOff1);
+
+        RequestedTimeOff requestedTimeOff2 = new RequestedTimeOff(LocalDate.of(2024,04,01),
+                LocalDate.of(2024,04, 2), Type.PARENTALLEAVE, "Think of the children!",
+                employee4);
+//          requestedTimeOffRepository.save(requestedTimeOff2);
+
+        RequestedTimeOff requestedTimeOff3 = new RequestedTimeOff(LocalDate.of(2025,06,13),
+                LocalDate.of(2025,06, 27), Type.HOLIDAYLEAVE, "Off to Portugal - " +
+                "tchau tá logo", employee3);
+
+        RequestedTimeOff requestedTimeOff4 = new RequestedTimeOff(LocalDate.of(2024,04,22),
+                LocalDate.of(2024,04, 22), Type.COMPASSIONATELEAVE,
+                "Husband cycled into a car door, in A&E",
+                employee4);
+
+        RequestedTimeOff requestedTimeOff5 = new RequestedTimeOff(LocalDate.of(2024,05,07),
+                LocalDate.of(2024,05, 17), Type.HOLIDAYLEAVE, "Visiting Madeira - " +
+                "I hope it tastes as good as the cake", employee2);
+
+        RequestedTimeOff requestedTimeOff6 = new RequestedTimeOff(LocalDate.of(2024,04,19),
+                LocalDate.of(2024,04, 20), Type.SICKLEAVE, "Got the squits", employee5);
     }
 }
