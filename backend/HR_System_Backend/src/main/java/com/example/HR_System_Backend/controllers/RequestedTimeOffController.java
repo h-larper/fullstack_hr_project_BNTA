460da8f38698731e2e39384f735756a13693d1e3
Laura Harper <lauraharper95@gmail.com>
2024-04-12 11:03:05 +0100

    created getMapping and method for all requested timeoffs

--- backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/controllers/RequestedTimeOffController.java
+++ backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/controllers/RequestedTimeOffController.java
@@ -1,8 +1,15 @@
 package com.example.HR_System_Backend.controllers;
 
+import com.example.HR_System_Backend.models.RequestedTimeOff;
+import com.example.HR_System_Backend.services.RequestedTimeOffService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/requested_time_off")
@@ -11,5 +18,10 @@ public class RequestedTimeOffController {
 
     @Autowired
     RequestedTimeOffService requestedTimeOffService;
+
+    @GetMapping
+    public ResponseEntity<List<RequestedTimeOff>> getAllRequestedTimeOffs() {
+        return new ResponseEntity<>(requestedTimeOffService.getAllRequestedTimeOffs(), HttpStatus.OK);
+    }
 
 }
--- backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/services/RequestedTimeOffService.java
+++ backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/services/RequestedTimeOffService.java
@@ -1,8 +1,11 @@
 package com.example.HR_System_Backend.services;
 
+import com.example.HR_System_Backend.models.RequestedTimeOff;
 import com.example.HR_System_Backend.repositories.RequestedTimeOffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RequestedTimeOffService {
@@ -10,4 +13,8 @@ public class RequestedTimeOffService {
     @Autowired
     RequestedTimeOffRepository requestedTimeOffRepository;
 
+    public List<RequestedTimeOff> getAllRequestedTimeOffs(){
+        return requestedTimeOffRepository.findAll();
+    }
+    
 }
