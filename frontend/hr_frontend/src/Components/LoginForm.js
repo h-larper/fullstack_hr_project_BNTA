4c978e0f5ff565e1b5fcbe124fdd3a8008d09eb7
Maya T <tettehmaya5@gmail.com>
2024-04-15 10:51:27 +0100

    Added form to login form file.

--- frontend/hr_frontend/src/Components/LoginForm.js
+++ frontend/hr_frontend/src/Components/LoginForm.js
@@ -1,6 +1,35 @@
+import { useState } from "react";
+
 const LoginForm = () => {
+
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+
     return ( 
-        <></>
+        <>
+            <h2>Login Form:</h2>
+
+            <form>
+                <label htmlFor="emailAddress-field">Email Address</label>
+                <input
+                    type="text"
+                    placeholder="Email Address"
+                    id="emailAddress-field"
+                    required
+                    value={emailAddress}
+                    onChange={(event) => setEmailAddress(event.target.value)}
+                />
+                <label htmlFor="password-field">Password</label>
+                <input
+                    type="text"
+                    placeholder="Password"
+                    id="password-field"
+                    required
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+            </form>
+        </>
      );
 }
  
