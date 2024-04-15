aa561db32c2f61c4b926f9ce65c3d28c26321183
Karen Yip <karenyip1999@gmail.com>
2024-04-15 16:29:53 +0100

    Added additional fields to holiday request form

--- frontend/hr_frontend/src/Components/HolidayRequestForm.js
+++ frontend/hr_frontend/src/Components/HolidayRequestForm.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 const HolidayRequestForm = () => {
 
-    const [date, setDate] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
     const [type, setType] = useState("");
     const [notes, setNotes] = useState("");
     const [value, setValue] = useState(null);
@@ -15,14 +16,25 @@ const HolidayRequestForm = () => {
         <>
             <p>HOLIDAY REQUESTS</p>
             <form>
+                <label htmlFor="start_date_field">Start Date</label>
                 <input
                     type = "date"
-                    label = "Date"
-                    placeholder="Date"
-                    value = {date}
-                    onChange={(event) => setDate(event.target.value)}
+                    placeholder="Start Date"
+                    value = {startDate}
+                    onChange={(event) => setStartDate(event.target.value)}
                     required
                 />
+
+                <label htmlFor="end_date_field">End Date</label>
+                <input
+                    type = "date"
+                    placeholder="End Date"
+                    value = {endDate}
+                    onChange={(event) => setEndDate(event.target.value)}
+                    required
+                />
+
+                <label htmlFor="type_field">Type</label>
                 <select  
                     id = "type"
                     onChange={handleChange}
@@ -34,6 +46,15 @@ const HolidayRequestForm = () => {
                     <option value = "other">Other</option>
                 </select>
 
+                <label htmlFor="notes_field">Notes</label>
+                <input 
+                    type = "text"
+                    placeholder="Notes"
+                    value = {notes}
+                    onChange={(event) => setNotes(event.target.value)}
+                    required
+                />
+                <input type="submit" value="Submit"/>
             </form>
         </>
      );
