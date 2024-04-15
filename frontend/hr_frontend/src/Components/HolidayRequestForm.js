a9daf03cfaffdd5a8038881ecaee84e2f6586f72
Karen Yip <karenyip1999@gmail.com>
2024-04-15 16:20:27 +0100

    Created holiday request form

--- frontend/hr_frontend/src/Components/HolidayRequestForm.js
+++ frontend/hr_frontend/src/Components/HolidayRequestForm.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const HolidayRequestForm = () => {
+
+    const [date, setDate] = useState("");
+    const [type, setType] = useState("");
+    const [notes, setNotes] = useState("");
+    const [value, setValue] = useState(null);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
     return ( 
         <>
             <p>HOLIDAY REQUESTS</p>
+            <form>
+                <input
+                    type = "date"
+                    label = "Date"
+                    placeholder="Date"
+                    value = {date}
+                    onChange={(event) => setDate(event.target.value)}
+                    required
+                />
+                <select  
+                    id = "type"
+                    onChange={handleChange}
+                >
+                    <option value = "sick_leave">Sick Leave</option>
+                    <option value = "compassionate_leave">Compassionate Leave</option>
+                    <option value = "parental_leave">Parental Leave</option>
+                    <option value = "holiday_leave">Holiday Leave</option>
+                    <option value = "other">Other</option>
+                </select>
+
+            </form>
         </>
      );
 }
