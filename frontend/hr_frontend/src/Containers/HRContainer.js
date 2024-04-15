7ca031cc373a89aa7b8fc5554636621a81354fb7
Maya T <tettehmaya5@gmail.com>
2024-04-15 10:07:38 +0100

    Added comments to HRContainer.js detailing what to include in file.

--- frontend/hr_frontend/src/Containers/HRContainer.js
+++ frontend/hr_frontend/src/Containers/HRContainer.js
@@ -1,4 +1,19 @@
 const HRContainer = () => {
+
+    // UseStates
+
+
+    // Fetch Requests
+
+
+    // UseEffects
+
+
+    // Other Functions
+
+
+    // Routes
+
     return ( 
         <h1>Hello from the container!</h1>
      );
