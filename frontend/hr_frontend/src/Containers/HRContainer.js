db743449335cb34bc6518d77e1a0b07340429ef0
Maya T <tettehmaya5@gmail.com>
2024-04-15 10:19:54 +0100

    Added currentUser state and fetch request.

--- frontend/hr_frontend/src/Containers/HRContainer.js
+++ frontend/hr_frontend/src/Containers/HRContainer.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 const HRContainer = () => {
 
     // UseStates
-
+    const [currentUser, setCurrentUser] = useState({});
 
     // Fetch Requests
-
+    const fetchCurrentUser = async () => {
+        const response = await fetch("http://localhost:8080/employees/login");
+        const data = await response.json();
+        setCurrentUser(data);
+    }
 
     // UseEffects
 
