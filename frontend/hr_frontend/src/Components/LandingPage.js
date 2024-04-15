d5cb406b3fe2f0e00d05a0cd64a2f2df9929c765
Hannah Riley <hannah.847@btinternet.com>
2024-04-15 14:56:37 +0100

    react modal for profile

--- frontend/hr_frontend/src/Components/LandingPage.js
+++ frontend/hr_frontend/src/Components/LandingPage.js
@@ -3,6 +3,7 @@ import Calendar from "./Calendar";
 import MyHolidaysList from "./MyHolidaysList";
 import ReactModal from "react-modal";
 import HolidayApprovalList from "./HolidayApprovalList";
+import HolidayRequestForm from "./HolidayRequestForm";
 
 const LandingPage = () => {
     
@@ -25,7 +26,8 @@ const LandingPage = () => {
             <Calendar />
             <MyHolidaysList />
 
-            <button onClick={toggleHolidayApprovalListModal}>Requests</button>
+            {/* Holiday Approvals list Modal */}
+            <button onClick={toggleHolidayApprovalListModal}>Approvals</button>
             <ReactModal
                 isOpen={holidayApprovalListModal}
                 onRequestClose={toggleHolidayApprovalListModal}
@@ -33,6 +35,28 @@ const LandingPage = () => {
             >
                 <HolidayApprovalList />
                 <button onClick={toggleHolidayApprovalListModal}>Close</button>
+            </ReactModal>
+
+            {/* Holiday Request Form Modal */}
+            <button onClick={toggleHolidayRequestModal}>Request Leave</button>
+            <ReactModal
+                isOpen={holidayRequestModal}
+                onRequestClose={toggleHolidayRequestModal}
+                ariaHideApp={false}
+            >
+                <HolidayRequestForm />
+                <button onClick={toggleHolidayRequestModal}>Close</button>
+            </ReactModal>
+
+            {/* Profile Modal */}
+            <button onClick={toggleProfileModal}>Profile</button>
+            <ReactModal
+                isOpen={ProfileModal}
+                onRequestClose={toggleProfileModal}
+                ariaHideApp={false}
+            >
+                <HolidayRequestForm />
+                <button onClick={toggleProfileModal}>Close</button>
             </ReactModal>
         </>
      );
