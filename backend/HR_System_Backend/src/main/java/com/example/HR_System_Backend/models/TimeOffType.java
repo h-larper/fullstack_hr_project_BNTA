043b085f7abfdf248400c2bcd9b9aead1feeaa1e
Maya T <tettehmaya5@gmail.com>
2024-04-16 14:11:18 +0100

    Amended back-end to change display name of enums.

--- backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/models/TimeOffType.java
+++ backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/models/TimeOffType.java
@@ -2,10 +2,23 @@ package com.example.HR_System_Backend.models;
 
 public enum TimeOffType {
 
-    SICKLEAVE,
-    COMPASSIONATELEAVE,
-    PARENTALLEAVE,
-    HOLIDAYLEAVE,
-    OTHER
+    SICKLEAVE("Sick Leave"),
+    COMPASSIONATELEAVE("Compassionate Leave"),
+    PARENTALLEAVE("Parental Leave"),
+    HOLIDAYLEAVE("Holiday Leave"),
+    OTHER("Other");
 
+    final String displayName;
+    TimeOffType(String displayName){
+        this.displayName = displayName;
+    }
+
+    public static TimeOffType valueOfDisplayName(String typeName){
+        for (TimeOffType timeOffType : TimeOffType.values()){
+            if (timeOffType.displayName.equals(typeName)){
+                return timeOffType;
+            }
+        }
+        return null;
+    }
 }
