6fe2705928be3a608daed8cf215635f2b3937534
Karen Yip <karenyip1999@gmail.com>
2024-04-17 12:46:28 +0100

    Added first name field in profile form

--- frontend/hr_frontend/src/Components/ProfileForm.js
+++ frontend/hr_frontend/src/Components/ProfileForm.js
@@ -1,7 +1,29 @@
+import currentUserContext from "./CurrentUserContext";
+import { useContext } from "react";
+
 const ProfileForm = () => {
+
+    const currentUser = useContext(currentUserContext);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return ( 
         <>
             <p>PROFILE</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="first-name-field">First Name: </label>
+                <input 
+                    id = "first-name-field"
+                    type = "text"
+                    placeholder={currentUser.currentUser.firstName}
+                    readOnly
+                />
+
+                
+            </form>
+
         </>
      );
 }
