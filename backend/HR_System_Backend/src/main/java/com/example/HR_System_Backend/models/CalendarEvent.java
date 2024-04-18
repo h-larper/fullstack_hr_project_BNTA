01a055948d1ba38c5aad132b41bec63dcbd1b6b8
Hannah Riley <hannah.847@btinternet.com>
2024-04-18 11:26:06 +0100

    create calendar event modal

---
+++ backend/HR_System_Backend/src/main/java/com/example/HR_System_Backend/models/CalendarEvent.java
@@ -0,0 +1,91 @@
+package com.example.HR_System_Backend.models;
+
+import java.time.LocalDate;
+
+public class CalendarEvent {
+
+    private Long id;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private TimeOffType timeOffType;
+
+    private String notes;
+
+    private String fullName;
+
+    private Boolean isCurrentUsersEvent;
+
+    public CalendarEvent() {
+    }
+
+    public CalendarEvent(Long id, LocalDate startDate, LocalDate endDate, TimeOffType timeOffType, String notes,
+                         String fullName, Boolean isCurrentUsersEvent) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.timeOffType = timeOffType;
+        this.notes = notes;
+        this.fullName = fullName;
+        this.isCurrentUsersEvent = isCurrentUsersEvent
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public TimeOffType getTimeOffType() {
+        return timeOffType;
+    }
+
+    public void setTimeOffType(TimeOffType timeOffType) {
+        this.timeOffType = timeOffType;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Boolean getCurrentUsersEvent() {
+        return isCurrentUsersEvent;
+    }
+
+    public void setCurrentUsersEvent(Boolean currentUsersEvent) {
+        isCurrentUsersEvent = currentUsersEvent;
+    }
+}
+
