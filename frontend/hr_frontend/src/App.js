bff7e7916a365a7ba37756501ec63d167a594e4e
Maya T <tettehmaya5@gmail.com>
2024-04-18 11:56:22 +0100

    changed font of footer.

--- frontend/hr_frontend/src/App.js
+++ frontend/hr_frontend/src/App.js
@@ -4,6 +4,11 @@ import HRContainer from './Containers/HRContainer';
 function App() {
   return (
     <>
+      <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/> 
+          <link href="https://fonts.googleapis.com/css2?family=Jersey+25&display=swap" rel="stylesheet"/>
+      </head>
       <HRContainer />
       <footer>
           Created by Lily's Subordinates ©️ so employees can escape once in a while! 🏝️
