433007a137478b4453cc17ba2d2a60c57bf2d2b4
Laura Harper <lauraharper95@gmail.com>
2024-04-18 10:14:15 +0100

    added footer to AppCss

--- frontend/hr_frontend/src/App.js
+++ frontend/hr_frontend/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <HRContainer />
+      <footer>
+          Created by Lily's Subordinates ©️ so employees can escape once in a while! 🏝️
+      </footer>
     </>
   );
 }
